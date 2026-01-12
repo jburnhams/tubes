a70a89e99634b3f6718ee62125f536d4c22b6b3c
Jonathan Burnhams <jburnhams@gmail.com>
2026-01-12 21:46:17 +0000

    Update CONFIG_URL to remove secret key
    Removed sensitive information from CONFIG_URL.

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,6 +1,6 @@
 import { ConfigResponse } from '../types/config';
 
-const CONFIG_URL = 'https://storage.jonathanburnhams.com/api/collections/2?secret=05dc8c899d57550315abd2926bdb7c44';
+const CONFIG_URL = 'https://storage.jonathanburnhams.com/api/collections/2';
 
 let cookieValue: string | null = null;
 
